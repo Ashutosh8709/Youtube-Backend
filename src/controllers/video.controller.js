3f9dd55a7f3cf3ae5579c7a36d0544c3dbf9e629
Ashutosh Kumar <ashutosh2003.sahi@gmail.com>
2025-09-18 14:38:53 +0530

    Implement video publishing functionality with error handling for missing fields and file uploads

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -21,6 +21,69 @@ const getAllVideos = asyncHandler(async (req, res) => {
 const publishAVideo = asyncHandler(async (req, res) => {
 	const { title, description } = req.body;
 	// TODO: get video, upload to cloudinary, create video
+	if (!title && !description) {
+		throw new ApiError(400, "Title or description is missing");
+	}
+
+	const userId = req.user?._id;
+
+	if (!userId) {
+		throw new ApiError(400, "User not Authenticated");
+	}
+
+	try {
+		const videoLocalPath = req.files?.videoFile[0]?.path;
+		const thumbnailLocalPath = req.files?.thumbnail[0]?.path;
+
+		if (!videoLocalPath || !thumbnailLocalPath) {
+			throw new ApiError(
+				400,
+				"Cant find Local Files of video or thumbnail"
+			);
+		}
+
+		const uploadedVideoFile = await uploadOnCloudinary(
+			videoLocalPath
+		);
+		const uploadedThumbnail = await uploadOnCloudinary(
+			thumbnailLocalPath
+		);
+
+		if (!uploadedVideoFile) {
+			throw new ApiError(
+				400,
+				"Error while uploading video on cloudinary"
+			);
+		}
+
+		if (!uploadedThumbnail) {
+			throw new ApiError(
+				400,
+				"Error while uploading thumbnail on cloudinary"
+			);
+		}
+
+		const video = await Video.create({
+			videoFile: uploadedVideoFile.secure_url,
+			thumbnail: uploadedThumbnail.secure_url,
+			title,
+			description,
+			duration: uploadedVideoFile.duration,
+			owner: userId,
+		});
+
+		return res
+			.status(200)
+			.json(
+				new ApiResponse(
+					200,
+					video,
+					"Video published successfully"
+				)
+			);
+	} catch (error) {
+		throw new ApiError(400, "Error while publishing a video");
+	}
 });
 
 const getVideoById = asyncHandler(async (req, res) => {

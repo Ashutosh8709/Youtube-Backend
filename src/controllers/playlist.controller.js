55c3459b4abe492e076c0f172dd9324e7c100a72
Ashutosh Kumar <ashutosh2003.sahi@gmail.com>
2025-09-19 14:30:28 +0530

    Refactor createPlaylist function to enhance error handling and ensure user ID validation

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -6,8 +6,58 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createPlaylist = asyncHandler(async (req, res) => {
 	const { name, description } = req.body;
-
 	//TODO: create playlist
+	const userId = req.user?._id;
+
+	if (!name && !description) {
+		throw new ApiError(
+			400,
+			"Name and Description both are required"
+		);
+	}
+
+	if (!userId) {
+		throw new ApiError(400, "User Id not found");
+	}
+
+	try {
+		const existingPlaylist = await Playlist.findOne({
+			name,
+			owner: userId,
+		});
+
+		if (existingPlaylist) {
+			throw new ApiError(
+				400,
+				"Playlist with this name is already present for this user"
+			);
+		}
+
+		const playlist = await Playlist.create({
+			name,
+			description,
+			owner: userId,
+		});
+
+		if (!playlist) {
+			throw new ApiError(400, "Error while saving plyalist");
+		}
+
+		return res
+			.status(200)
+			.json(
+				new ApiResponse(
+					200,
+					playlist,
+					"Playlist created Successfully"
+				)
+			);
+	} catch (error) {
+		throw new ApiError(
+			400,
+			"Error occured while creating playlist"
+		);
+	}
 });
 
 const getUserPlaylists = asyncHandler(async (req, res) => {

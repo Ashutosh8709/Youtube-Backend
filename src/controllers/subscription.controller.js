0fcc179bef8e31452095b2422c13c9f520dfce7d
Ashutosh Kumar <ashutosh2003.sahi@gmail.com>
2025-09-19 12:46:40 +0530

    Refactor getUserChannelSubscribers function to improve error handling and streamline subscriber retrieval

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -62,13 +62,37 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 	}
 });
 
-// controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+	// controller to return subscriber list of a channel
 	const { channelId } = req.params;
+	if (!channelId) {
+		throw new ApiError(400, "Channel id not found");
+	}
+
+	try {
+		const subscribers = await Subscription.find({
+			channel: channelId,
+		});
+
+		return res
+			.status(200)
+			.json(
+				new ApiResponse(
+					200,
+					subscribers,
+					"Subscribers Fetched Successfully"
+				)
+			);
+	} catch (error) {
+		throw new ApiError(
+			400,
+			"Error occured while fetching channel subscribers"
+		);
+	}
 });
 
-// controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
+	// controller to return channel list to which user has subscribed
 	const { subscriberId } = req.params;
 });
 
